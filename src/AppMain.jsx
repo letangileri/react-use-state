ab0d8cc433320c49fa4f92ac11b6993fc1348237
letangileri <let.angileri@gmail.com>
2025-10-02 11:50:20 +0200

    add bonus one and two

--- src/AppMain.jsx
+++ src/AppMain.jsx
@@ -3,7 +3,9 @@ import { useState } from "react";
 
 export default function AppMain(){
 
-    const [currentTextId, setCurrentTextId] = useState(1);
+    const [currentTextId, setCurrentTextId] = useState(0);
+    const [active, setActive] = useState(false);
+
 
     console.log(setCurrentTextId);
     
@@ -11,25 +13,28 @@ export default function AppMain(){
     const getObj = languages.find(language => {
         return currentTextId == language.id
     })
-    console.log(getObj.id);
+    // console.log(getObj.id);
 
     function handleClick(id){
         console.log("ciao", id);
+        console.log(currentTextId);
         // const currentlyClickedItem = e.target.getAttribute('data-id');
         // console.log(currentlyClickedItem);
         setCurrentTextId(id);
+        console.log(currentTextId);
+        setActive(!active);
         
     }
     return(
         <>
         <div className="buttons">
         {languages.map(singleLanguage => (           
-            <button key = {singleLanguage.id} onClick={()=>handleClick(singleLanguage.id)}>{singleLanguage.title} </button>            
+            <button key = {singleLanguage.id} onClick={()=>handleClick(singleLanguage.id)} style={{ backgroundColor: active && currentTextId == singleLanguage.id ? "yellow" : "white" }}>{singleLanguage.title} </button>            
         ))}
         </div>
 
 
-       {            
+       {currentTextId == 0 ? <p> Nessun linguaggio selezionato </p> :         
         <div key={getObj.id}>
             <h2>{getObj.title}</h2>
             <p>{getObj.description}</p>
